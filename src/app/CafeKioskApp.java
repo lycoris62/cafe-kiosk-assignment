5f30f84516d0b0a4f60c953b4f453b637776f119
barami62 <wodbs7758@naver.com>
2023-10-19 11:12:55 +0900

    feat : Input 객체 주입

--- src/app/CafeKioskApp.java
+++ src/app/CafeKioskApp.java
@@ -1,21 +1,25 @@
 package app;
 
-public class CafeKioskApp {
-	private final String reader;
+import app.io.input.Input;
+import app.io.input.ScannerInput;
 
-	private CafeKioskApp(String  reader) {
-		this.reader = reader;
+public class CafeKioskApp {
+	private final Input input;
+
+	public CafeKioskApp(Input input) {
+		this.input = input;
 	}
 
 	public static void run() {
-		CafeKioskApp app = new CafeKioskApp("scanner");
+		CafeKioskApp app = new CafeKioskApp(new ScannerInput());
 		app.start();
 	}
 
 	private void start() {
 		while (true) {
 			try {
-
+				String str = input.read();
+				System.out.println("str = " + str);
 			} catch (IllegalArgumentException e) {
 				System.out.println("잘못된 입력으로 메인 메뉴판 화면으로 이동합니다.");
 			}
