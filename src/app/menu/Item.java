0c6ebb5cd46be85d148a69c55aca0ec05184b18e
barami62 <wodbs7758@naver.com>
2023-10-20 12:36:03 +0900

    feat : 상품 객체 기능 추가

--- src/app/menu/Item.java
+++ src/app/menu/Item.java
@@ -1,11 +1,20 @@
 package app.menu;
 
-public class Item extends MenuItem{
+public class Item extends MenuItem {
 
 	private final int price;
 
-	public Item(String name, String description, int price) {
-		super(name, description);
-		this.price = price;
+	public Item(CategoryMenu categoryMenu) {
+		super(categoryMenu.getMenuName(), categoryMenu.getDescription());
+		this.price = categoryMenu.getPrice();
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%-15s | ₩ %2.1f | %s", name, price / 1000.0, description);
 	}
 }
--- src/app/menu/MenuItem.java
+++ src/app/menu/MenuItem.java
@@ -1,11 +1,19 @@
 package app.menu;
 
 public class MenuItem {
-	private final String name;
-	private final String description;
+	protected final String name;
+	protected final String description;
 
 	public MenuItem(String name, String description) {
 		this.name = name;
 		this.description = description;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDescription() {
+		return description;
 	}
 }
