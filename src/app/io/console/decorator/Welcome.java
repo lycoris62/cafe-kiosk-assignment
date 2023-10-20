1ea378cca6284ded0d81ba6b2ceee50655f1ba9a
barami62 <wodbs7758@naver.com>
2023-10-20 12:40:01 +0900

    feat : 메인 메뉴판 화면 데코레이터 추가

---
+++ src/app/io/console/decorator/Welcome.java
@@ -0,0 +1,58 @@
+package app.io.console.decorator;
+
+import app.io.console.Console;
+import app.io.console.ConsoleDecorator;
+import app.menu.Category;
+
+public class Welcome extends ConsoleDecorator {
+
+	private final String initMessage = """
+		"오십이도에 오신걸 환영합니다."
+		아래 메뉴판을 보시고 메뉴를 골라 입력해주세요.
+				
+		""";
+	private final String titleFormat = "[ %s MENU ]\n";
+	private final String mainFormat = "%d. %-20s | %s";
+
+	public Welcome(Console console) {
+		super(console);
+	}
+
+	@Override
+	public int request() {
+		showMainMenu();
+		int input = super.request();
+		validate(input);
+
+		return input;
+	}
+
+	private void showMainMenu() {
+		StringBuilder sb = new StringBuilder("\n");
+		sb.append(initMessage);
+		sb.append(String.format(titleFormat, "오십이도"));
+
+		int sequence = 1;
+
+		for (Category category : Category.values()) {
+			sb.append(sequence++).append(". ")
+				.append(category.toString())
+				.append("\n");
+		}
+
+		sb.append("\n").append(String.format(titleFormat, "ORDER"));
+		sb.append(String.format(mainFormat, sequence++, "Order", "장바구니를 확인 후 주문합니다."))
+			.append("\n")
+			.append(String.format(mainFormat, sequence, "Cancel", "진행중인 주문을 취소합니다."));
+
+		System.out.println(sb);
+	}
+
+	private void validate(int input) {
+		int menuCount = Category.values().length + 2;
+
+		if (input < 0 || menuCount < input) {
+			throw new IllegalArgumentException();
+		}
+	}
+}
