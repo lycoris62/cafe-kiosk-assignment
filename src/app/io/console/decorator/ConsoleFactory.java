fd99da23746ce248e56fb4ae1063e1f42f8c1a6b
barami62 <wodbs7758@naver.com>
2023-10-20 14:03:59 +0900

    feat : 콘솔팩토리 추가
    항상 쓰이는 BaseConsole(Input) 을 계속 재사용

---
+++ src/app/io/console/decorator/ConsoleFactory.java
@@ -0,0 +1,43 @@
+package app.io.console.decorator;
+
+import java.util.List;
+
+import app.io.console.BaseConsole;
+import app.io.console.Console;
+import app.io.input.Input;
+import app.menu.Order;
+
+public class ConsoleFactory {
+
+	private final Input input;
+	private final Console baseConsole;
+
+	public ConsoleFactory(Input input) {
+		this.input = input;
+		this.baseConsole = new BaseConsole(input);
+	}
+
+	public Console getWelcome() {
+		return new Welcome(baseConsole);
+	}
+
+	public Console getRecord(List<Order> salesRecord) {
+		return new Record(baseConsole, salesRecord);
+	}
+
+	public Console getOrderCancel() {
+		return new OrderCancel(baseConsole);
+	}
+
+	public Console getOrderProcess(List<Order> cart) {
+		return new OrderProcess(baseConsole, cart);
+	}
+
+	public Console getCategoryMenu(int categoryNumber) {
+		return new CategoryMenu(baseConsole, categoryNumber);
+	}
+
+	public Console getPurchase(int categoryNumber, int itemNumber) {
+		return new Purchase(baseConsole, categoryNumber, itemNumber);
+	}
+}
