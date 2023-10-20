48f62a94d3c2734041d0dd5df373591b2fd01c49
barami62 <wodbs7758@naver.com>
2023-10-20 12:32:08 +0900

    feat : 메뉴 enum 기능 추가

--- src/app/menu/Category.java
+++ src/app/menu/Category.java
@@ -5,22 +5,55 @@ import static app.menu.CategoryMenu.*;
 import java.util.List;
 
 public enum Category {
-	COFFEE("Coffee",
-		List.of(AMERICANO, EINSPANNER, CAFE_LATTE, CAFE_LATTE, VANILLA_LATTE, OREO_LATTE)),
-	ADE("Ade",
-		List.of(GREEN_GRAPE_ADE, GRAPE_FRUIT_ADE, LEMON_ADE, MANGO_ADE, STRAWBERRY_ADE, BLUEBERRY_ADE)),
-	TEA("Tea", List.of(STRAWBERRY_TEA)),
-	LATTE("Latte",
-		List.of(GREEN_TEA_LATTE, EARL_GREY_MILK_TEA, MANGO_CREAM_LATTE, STRAWBERRY_CREAM_LATTE)),
-	HANDMADE_DESSERT("Handmade Dessert",
-		List.of(WHIPPED_CREAM_CROISSANT, GREEN_GRAPE_CROISSANT, OREO_CROISSANT,
-			CHOCO_CROISSANT, STRAWBERRY_CROISSANT, PLAIN_SCONE, EARL_GREY_CORN));
+	COFFEE("Coffee", "커피 입니다.", List.of(
+		AMERICANO,
+		EINSPANNER,
+		CAFE_LATTE,
+		VANILLA_LATTE,
+		OREO_LATTE)),
+
+	ADE("Ade", "에이드 입니다.", List.of(
+		GREEN_GRAPE_ADE,
+		GRAPE_FRUIT_ADE,
+		LEMON_ADE,
+		MANGO_ADE,
+		STRAWBERRY_ADE,
+		BLUEBERRY_ADE)),
+
+	TEA("Tea", "차 입니다.", List.of(STRAWBERRY_TEA)),
+
+	LATTE("Latte", "라떼 입니다.", List.of(
+		GREEN_TEA_LATTE,
+		EARL_GREY_MILK_TEA,
+		MANGO_CREAM_LATTE,
+		STRAWBERRY_CREAM_LATTE)),
+
+	HANDMADE_DESSERT("Handmade Dessert", "수제 디저트 입니다.", List.of(
+		WHIPPED_CREAM_CROISSANT,
+		GREEN_GRAPE_CROISSANT,
+		OREO_CROISSANT,
+		CHOCO_CROISSANT,
+		STRAWBERRY_CROISSANT,
+		PLAIN_SCONE,
+		EARL_GREY_CORN));
 
 	private final String menuName;
-	private final List<CategoryMenu> categoryItems;
+	private final String description;
+	private final List<app.menu.CategoryMenu> categoryItems;
 
-	Category(String menuName, List<CategoryMenu> categoryItems) {
+	Category(String menuName, String description, List<app.menu.CategoryMenu> categoryItems) {
 		this.menuName = menuName;
+		this.description = description;
 		this.categoryItems = categoryItems;
+	}
+
+	public static List<CategoryMenu> getCategoryItems(int categoryNumber) {
+		Category category = Category.values()[categoryNumber - 1];
+		return category.categoryItems;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%-20s | %s", menuName, description);
 	}
 }
--- src/app/menu/CategoryMenu.java
+++ src/app/menu/CategoryMenu.java
@@ -3,38 +3,62 @@ package app.menu;
 public enum CategoryMenu {
 
 	// Coffee
-	AMERICANO("아메리카노", 4000),
-	EINSPANNER("아인슈페너", 5000),
-	CAFE_LATTE("카페 라떼", 4300),
-	VANILLA_LATTE("바닐라 라떼", 4500),
-	OREO_LATTE("오레오 라떼", 4800),
+	AMERICANO("아메리카노", 4000, "아메리카노 입니다."),
+	EINSPANNER("아인슈페너", 5000, "아인슈페너 입니다."),
+	CAFE_LATTE("카페 라떼", 4300, "카페 라떼 입니다."),
+	VANILLA_LATTE("바닐라 라떼", 4500, "바닐라 라떼 입니다."),
+	OREO_LATTE("오레오 라떼", 4800, "오레오 라떼 입니다."),
 
 	// Ade
-	GREEN_GRAPE_ADE("청포도 에이드", 5500),
-	GRAPE_FRUIT_ADE("자몽 에이드", 5500),
-	LEMON_ADE("레몬 에이드", 5500),
-	MANGO_ADE("망고 에이드", 6000),
-	STRAWBERRY_ADE("딸기 에이드", 6000),
-	BLUEBERRY_ADE("블루베리 에이드", 6000),
+	GREEN_GRAPE_ADE("청포도 에이드", 5500, "청포도 에이드 입니다."),
+	GRAPE_FRUIT_ADE("자몽 에이드", 5500, "자몽 에이드 입니다."),
+	LEMON_ADE("레몬 에이드", 5500, "레몬 에이드 입니다."),
+	MANGO_ADE("망고 에이드", 6000, "망고 에이드 입니다."),
+	STRAWBERRY_ADE("딸기 에이드", 6000, "딸기 에이드 입니다."),
+	BLUEBERRY_ADE("블루베리 에이드", 6000, "블루베리 에이드 입니다."),
 
 	// Tea
-	STRAWBERRY_TEA("딸기차", 4500),
+	STRAWBERRY_TEA("딸기차", 4500, "딸기차 입니다."),
 
 	// Latte
-	GREEN_TEA_LATTE("녹차 라떼", 4500),
-	EARL_GREY_MILK_TEA("얼그레이 홍차 라떼", 4500),
-	MANGO_CREAM_LATTE("망고 크림 라떼", 6000),
-	STRAWBERRY_CREAM_LATTE("딸기 크림 라떼", 6000),
+	GREEN_TEA_LATTE("녹차 라떼", 4500, "녹차 라떼 입니다."),
+	EARL_GREY_MILK_TEA("얼그레이 홍차 라떼", 4500, "얼그레이 홍차 라떼 입니다."),
+	MANGO_CREAM_LATTE("망고 크림 라떼", 6000, "망고 크림 라떼 입니다."),
+	STRAWBERRY_CREAM_LATTE("딸기 크림 라떼", 6000, "딸기 크림 라떼 입니다."),
 
 	// Handmade Dessert
-	WHIPPED_CREAM_CROISSANT("생크림 크루아상", 4500),
-	GREEN_GRAPE_CROISSANT("청포도 크루아상", 5000),
-	OREO_CROISSANT("오레오 크루아상", 5000),
-	CHOCO_CROISSANT("초코 크루아상", 5000),
-	STRAWBERRY_CROISSANT("딸기 크루아상", 5500),
-	PLAIN_SCONE("플레인 스콘", 3000),
-	EARL_GREY_CORN("얼그레이 스콘", 3400);
+	WHIPPED_CREAM_CROISSANT("생크림 크루아상", 4500, "생크림 크루아상 입니다."),
+	GREEN_GRAPE_CROISSANT("청포도 크루아상", 5000, "청포도 크루아상 입니다."),
+	OREO_CROISSANT("오레오 크루아상", 5000, "오레오 크루아상 입니다."),
+	CHOCO_CROISSANT("초코 크루아상", 5000, "초코 크루아상 입니다."),
+	STRAWBERRY_CROISSANT("딸기 크루아상", 5500, "딸기 크루아상 입니다."),
+	PLAIN_SCONE("플레인 스콘", 3000, "플레인 스콘 입니다."),
+	EARL_GREY_CORN("얼그레이 스콘", 3400, "얼그레이 스콘 입니다.");
 
-	CategoryMenu(String menuName, int price) {
+	private final String menuName;
+	private final int price;
+	private final String description;
+
+	CategoryMenu(String menuName, int price, String description) {
+		this.menuName = menuName;
+		this.price = price;
+		this.description = description;
+	}
+
+	public String getMenuName() {
+		return menuName;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%-15s | ₩ %2.1f | %s", menuName, price / 1000.0, description);
 	}
 }
