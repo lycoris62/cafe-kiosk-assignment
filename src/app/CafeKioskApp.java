1189d204a4e648c21c2b7e234ae94045759a7527
barami62 <wodbs7758@naver.com>
2023-10-21 23:23:49 +0900

    refactor : 핸들러 추가로 필요없어진 코드 제거

--- src/app/CafeKioskApp.java
+++ src/app/CafeKioskApp.java
@@ -1,20 +1,10 @@
 package app;
 
 import app.command.CommandRunner;
-import app.command.implement.OrderCancelCommand;
-import app.command.implement.OrderCommand;
-import app.command.implement.PurchaseCommand;
-import app.command.implement.SalesRecordCommand;
-import app.menu.Category;
 
 public class CafeKioskApp {
-	private final CommandRunner runner = new CommandRunner();
 
-	private final int SALES_RECORD_NUMBER = 0;
-	private final int MENU_START_NUMBER = 1;
-	private final int MENU_END_NUMBER = Category.values().length;
-	private final int ORDER_NUMBER = Category.values().length + 1;
-	private final int ORDER_CANCEL_NUMBER = Category.values().length + 2;
+	private final CommandRunner commandRunner = new CommandRunner();
 
 	private CafeKioskApp() {}
 
@@ -26,25 +16,12 @@ public class CafeKioskApp {
 	private void start() {
 		while (true) {
 			try {
-				int menuNumber = runner.init();
-				setCommand(menuNumber);
-				runner.run();
+				commandRunner.init();
+				commandRunner.run();
 			} catch (IllegalArgumentException e) {
 				System.out.println("잘못된 입력으로 3초 뒤 메인 메뉴판 화면으로 이동합니다.");
 				waitForThreeSeconds();
 			}
-		}
-	}
-
-	private void setCommand(int menuNumber) {
-		if (menuNumber == SALES_RECORD_NUMBER) {
-			runner.setCommand(new SalesRecordCommand());
-		} else if (MENU_START_NUMBER <= menuNumber && menuNumber <= MENU_END_NUMBER) {
-			runner.setCommand(new PurchaseCommand(menuNumber));
-		} else if (menuNumber == ORDER_NUMBER) {
-			runner.setCommand(new OrderCommand());
-		} else if (menuNumber == ORDER_CANCEL_NUMBER) {
-			runner.setCommand(new OrderCancelCommand());
 		}
 	}
 
