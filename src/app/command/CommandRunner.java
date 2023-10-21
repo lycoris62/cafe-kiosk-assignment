573a784eb5f33e67ac533fd82391e1f1ac265c9e
barami62 <wodbs7758@naver.com>
2023-10-21 23:23:25 +0900

    feat : 메인 메뉴판 화면 커맨드에 핸들러 추가

--- src/app/command/CommandRunner.java
+++ src/app/command/CommandRunner.java
@@ -1,6 +1,11 @@
 package app.command;
 
 import app.command.implement.WelcomeCommand;
+import app.handler.Handler;
+import app.handler.implement.OrderCancelHandler;
+import app.handler.implement.OrderHandler;
+import app.handler.implement.PurchaseHandler;
+import app.handler.implement.SalesRecordHandler;
 
 public class CommandRunner {
 
@@ -14,8 +19,19 @@ public class CommandRunner {
 		command.execute();
 	}
 
-	public int init() {
+	public void init() {
 		setCommand(new WelcomeCommand());
-		return command.console.request();
+		int menuNumber = command.console.request();
+
+		Handler rootHandler = settingHandler();
+		rootHandler.run(this, menuNumber);
+	}
+	private Handler settingHandler() {
+		Handler rootHandler = new SalesRecordHandler();
+		rootHandler
+			.setNext(new PurchaseHandler())
+			.setNext(new OrderHandler())
+			.setNext(new OrderCancelHandler());
+		return rootHandler;
 	}
 }
