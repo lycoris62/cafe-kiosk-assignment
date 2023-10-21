dbae2921e8b7318a8842290f38710376658c342d
barami62 <wodbs7758@naver.com>
2023-10-21 22:43:42 +0900

    refactor : 커맨드에서 콘솔팩토리를 관리하도록 포함

--- src/app/command/Command.java
+++ src/app/command/Command.java
@@ -1,9 +1,11 @@
 package app.command;
 
 import app.io.console.Console;
+import app.io.console.ConsoleFactory;
 
 public abstract class Command {
 
 	protected Console console;
+	protected ConsoleFactory consoleFactory = new ConsoleFactory();
 	public abstract void execute();
 }
