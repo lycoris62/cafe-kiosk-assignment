0e72281aa20c6eaa8c1be6dd57763c6959e56d4a
barami62 <wodbs7758@naver.com>
2023-10-22 12:10:49 +0900

    refactor : 기능별로 메서드를 나눔

--- src/app/command/implement/OrderCommand.java
+++ src/app/command/implement/OrderCommand.java
@@ -7,6 +7,7 @@ import app.container.SalesRecord;
 public class OrderCommand extends Command {
 
 	private static int waitingNumber = 1;
+	private final int ORDER_CANCEL_NUMBER = 2;
 
 	public OrderCommand() {
 		this.console = consoleFactory.getOrderProcess();
@@ -17,23 +18,38 @@ public class OrderCommand extends Command {
 		Cart cart = Cart.getInstance();
 		SalesRecord salesRecord = SalesRecord.getInstance();
 
+		if (isCartEmpty(cart)) {
+			return;
+		}
+		if (confirmOrderCancel()) {
+			return;
+		}
+
+		orderFinishProcess(salesRecord, cart);
+	}
+
+	private boolean confirmOrderCancel() {
+		int checkNumber = console.request();
+		return checkNumber == ORDER_CANCEL_NUMBER;
+	}
+
+	private boolean isCartEmpty(Cart cart) {
 		if (cart.isEmpty()) {
 			System.out.println("장바구니가 비어있습니다.");
-			return;
+			return true;
 		}
-
-		int checkNumber = console.request();
-
-		if (checkNumber == 2) {
-			return;
-		}
-
+		return false;
+	}
+	private void showOrderFinishMessage() {
 		System.out.println("주문이 완료되었습니다!\n");
 		System.out.printf("""
 						대기번호는 [ %d ] 번 입니다.
 						(3초후 메뉴판으로 돌아갑니다.)
 						""", waitingNumber++);
+	}
 
+	private void orderFinishProcess(SalesRecord salesRecord, Cart cart) {
+		showOrderFinishMessage();
 		salesRecord.record(cart.getAllOrders());
 		cart.clear();
 
