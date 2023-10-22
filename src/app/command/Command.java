a7fad70623107091782dfe6a54fe17cbd5ef25b4
barami62 <wodbs7758@naver.com>
2023-10-22 14:31:32 +0900

    refactor : Command 에 필요 없어진 콘솔 객체 제거

--- src/app/command/Command.java
+++ src/app/command/Command.java
@@ -1,11 +1,5 @@
 package app.command;
 
-import app.console.Console;
-import app.console.ConsoleFactory;
-
 public abstract class Command {
-
-	protected Console console;
-	protected ConsoleFactory consoleFactory = new ConsoleFactory();
 	public abstract void execute();
 }
