9ad6d7570e742df4f2b43ed26ac5ce70155b2999
barami62 <wodbs7758@naver.com>
2023-10-22 12:00:02 +0900

    refactor : 기능별로 메서드를 나눔

--- src/app/command/implement/OrderCancelCommand.java
+++ src/app/command/implement/OrderCancelCommand.java
@@ -5,6 +5,8 @@ import app.container.Cart;
 
 public class OrderCancelCommand extends Command {
 
+	private final int ORDER_CANCEL_AGREE_NUMBER = 1;
+
 	public OrderCancelCommand() {
 		this.console = consoleFactory.getOrderCancel();
 	}
@@ -12,10 +14,18 @@ public class OrderCancelCommand extends Command {
 	@Override
 	public void execute() {
 		int checkNumber = this.console.request();
-		if (checkNumber == 1) {
-			Cart cart = Cart.getInstance();
-			cart.clear();
+		checkOrderCancel(checkNumber);
+	}
+
+	private void checkOrderCancel(int checkNumber) {
+		if (checkNumber == ORDER_CANCEL_AGREE_NUMBER) {
+			putCart();
 			System.out.println("진행하던 주문이 취소되었습니다.\n");
 		}
+	}
+
+	private void putCart() {
+		Cart cart = Cart.getInstance();
+		cart.clear();
 	}
 }
