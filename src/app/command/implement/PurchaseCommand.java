bb549839f002e5157506b394a99af56a3c81816a
barami62 <wodbs7758@naver.com>
2023-10-22 11:57:16 +0900

    refactor : 기능별로 메서드를 나눔

--- src/app/command/implement/PurchaseCommand.java
+++ src/app/command/implement/PurchaseCommand.java
@@ -12,6 +12,7 @@ import app.menu.Order;
 public class PurchaseCommand extends Command {
 
 	private final int categoryNumber;
+	private final int PURCHASE_AGREE_NUMBER = 1;
 
 	public PurchaseCommand(int categoryNumber) {
 		this.categoryNumber = categoryNumber;
@@ -19,21 +20,48 @@ public class PurchaseCommand extends Command {
 
 	@Override
 	public void execute() {
-		console = consoleFactory.getCategoryMenu(categoryNumber);
-		int itemNumber = console.request();
+		int itemNumber = getItemNumber();
+		int purchaseNumber = getCheckNumber(itemNumber);
+		checkPurchase(purchaseNumber, itemNumber);
+	}
 
-		console = consoleFactory.getPurchase(categoryNumber, itemNumber);
-		int purchaseNumber = console.request();
-
-		if (purchaseNumber == 1) {
-			Product product = Category.getItem(categoryNumber, itemNumber);
-			Item item = new Item(product);
-			Order order = new Order(List.of(item));
-
-			System.out.println(order.getItemList().get(0).getName() + " 가 장바구니에 추가되었습니다.");
-
-			Cart cart = Cart.getInstance();
-			cart.put(order);
+	private void checkPurchase(int purchaseNumber, int itemNumber) {
+		if (purchaseNumber == PURCHASE_AGREE_NUMBER) {
+			Order order = orderProcess(itemNumber);
+			putCart(order);
 		}
+	}
+
+	private void showAllOrderedItem(Order order) {
+		for (Item item : order.getItemList()) {
+			System.out.println(item.getName() + " 가 장바구니에 추가되었습니다.");
+		}
+	}
+
+	private int getItemNumber() {
+		console = consoleFactory.getCategoryMenu(categoryNumber);
+		return console.request();
+	}
+
+	private int getCheckNumber(int itemNumber) {
+		console = consoleFactory.getPurchase(categoryNumber, itemNumber);
+		return console.request();
+	}
+
+	private Order createOrder(int itemNumber) {
+		Product product = Category.getItem(categoryNumber, itemNumber);
+		Item item = new Item(product);
+		return new Order(List.of(item));
+	}
+
+	private Order orderProcess(int itemNumber) {
+		Order order = createOrder(itemNumber);
+		showAllOrderedItem(order);
+		return order;
+	}
+
+	private void putCart(Order order) {
+		Cart cart = Cart.getInstance();
+		cart.put(order);
 	}
 }
