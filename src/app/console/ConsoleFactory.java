3106f1d8462926a6bcdecb7aff7889b3d3ed4c49
barami62 <wodbs7758@naver.com>
2023-10-22 14:26:57 +0900

    refactor : 콘솔 팩토리를 static 으로 변경

--- src/app/console/ConsoleFactory.java
+++ src/app/console/ConsoleFactory.java
@@ -1,42 +1,40 @@
 package app.console;
 
-import app.console.decorator.Purchase;
-import app.console.input.ScannerInput;
 import app.console.decorator.ItemMenu;
 import app.console.decorator.OrderCancel;
 import app.console.decorator.OrderProcess;
+import app.console.decorator.Purchase;
 import app.console.decorator.Record;
 import app.console.decorator.Welcome;
 
 public class ConsoleFactory {
 
-	private final Console baseConsole;
+	private static final Console baseConsole = new BaseConsole();
 
 	public ConsoleFactory() {
-		this.baseConsole = new BaseConsole(new ScannerInput());
 	}
 
-	public Console getWelcome() {
+	public static Console getWelcome() {
 		return new Welcome(baseConsole);
 	}
 
-	public Console getRecord() {
+	public static Console getRecord() {
 		return new Record(baseConsole);
 	}
 
-	public Console getOrderCancel() {
+	public static Console getOrderCancel() {
 		return new OrderCancel(baseConsole);
 	}
 
-	public Console getOrderProcess() {
+	public static Console getOrderProcess() {
 		return new OrderProcess(baseConsole);
 	}
 
-	public Console getCategoryMenu(int categoryNumber) {
+	public static Console getCategoryMenu(int categoryNumber) {
 		return new ItemMenu(baseConsole, categoryNumber);
 	}
 
-	public Console getPurchase(int categoryNumber, int itemNumber) {
+	public static Console getPurchase(int categoryNumber, int itemNumber) {
 		return new Purchase(baseConsole, categoryNumber, itemNumber);
 	}
 }
