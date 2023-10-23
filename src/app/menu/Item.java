3af730206e3c0074f8699d98a038de0bddead6d6
barami62 <wodbs7758@naver.com>
2023-10-23 18:30:47 +0900

    feat : Map이 같은 Item을 구별하도록 equals와 hashcode 재정의

--- src/app/menu/Item.java
+++ src/app/menu/Item.java
@@ -1,5 +1,7 @@
 package app.menu;
 
+import java.util.Objects;
+
 public class Item extends Menu {
 
 	private final int price;
@@ -14,7 +16,38 @@ public class Item extends Menu {
 	}
 
 	@Override
+	public boolean equals(Object obj) {
+		if (obj == this) {
+			return true;
+		}
+		if (obj instanceof Item item) {
+			return isPropertyEqualed(item);
+		}
+
+		return false;
+	}
+
+	private boolean isPropertyEqualed(Item item) {
+		if (!item.getName().equals(this.name)){
+			return false;
+		}
+		if (!item.getDescription().equals(this.description)){
+			return false;
+		}
+		return item.getPrice() == this.price;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, description, price);
+	}
+
+	@Override
 	public String toString() {
 		return String.format("%-15s | ₩ %2.1f | %s", name, price / 1000.0, description);
+	}
+
+	public String toStringWithCount(int itemCount) {
+		return String.format("%-15s | ₩ %3.1f | %2d개 | %-20s", name, price / 1000.0, itemCount, description);
 	}
 }
