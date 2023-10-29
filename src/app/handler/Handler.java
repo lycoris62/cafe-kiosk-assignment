80b5e08c49e70ef463510732750940654fd16f08
barami62 <wodbs7758@naver.com>
2023-10-30 01:34:39 +0900

    refactor : 핸들러를 실행하는 함수를 직관적으로 변경

--- src/app/handler/Handler.java
+++ src/app/handler/Handler.java
@@ -15,12 +15,12 @@ public abstract class Handler {
 
 	protected abstract void process(CommandRunner runner, int menuNumber);
 
-	public void run(CommandRunner runner, int menuNumber) {
+	public void handle(CommandRunner runner, int menuNumber) {
 		if (check(menuNumber)) {
 			process(runner, menuNumber);
 			return;
 		}
 
-		nextHandler.run(runner, menuNumber);
+		nextHandler.handle(runner, menuNumber);
 	}
 }
