a3cad853a71291fd16cb4ef72319717b5a932308
barami62 <wodbs7758@naver.com>
2023-10-30 17:08:19 +0900

    refactor : 문자열을 리턴하는 메서드를 직접 출력으로 변경

--- src/app/menu/MenuView.java
+++ src/app/menu/MenuView.java
@@ -1,11 +1,14 @@
 package app.menu;
 
 import java.util.Arrays;
-import java.util.stream.Collectors;
 
 import app.domain.Category;
+import app.domain.Product;
 
 public class MenuView {
+
+	private static final int ORDER_NUMBER = Category.size() + 1;
+	private static final int ORDER_CANCEL_NUMBER = Category.size() + 2;
 
 	private static final String initMessage = """
 		  
@@ -16,27 +19,27 @@ public class MenuView {
 	public void showMainMenuMessage() {
 		System.out.println(initMessage);
 		System.out.println("[ 오십이도 MENU ]");
-		System.out.println(getCategoryView());
-		System.out.printf("%d. %-20s | %-20s\n", Category.size() + 1, "Order", "장바구니를 확인 후 주문합니다.");
-		System.out.printf(String.format("%d. %-20s | %-20s\n", Category.size() + 2, "Cancel", "진행중인 주문을 취소합니다."));
+		showAllCategory();
+		System.out.printf("%d. %-20s | %-20s\n", ORDER_NUMBER, "Order", "장바구니를 확인 후 주문합니다.");
+		System.out.printf(String.format("%d. %-20s | %-20s\n", ORDER_CANCEL_NUMBER, "Cancel", "진행중인 주문을 취소합니다."));
 	}
 
-	private String getCategoryView() {
-		return Arrays.stream(Category.values())
-			.map(category -> category.toString() + "\n")
-			.collect(Collectors.joining());
+	private void showAllCategory() {
+		Arrays.stream(Category.values())
+			.map(Category::toString)
+			.forEach(System.out::println);
 	}
 
 	public void showCategoryMenuMessage(int categoryNumber) {
 		System.out.println(initMessage);
 		System.out.printf(String.format("[ %s MENU ]\n", Category.getCategoryName(categoryNumber)));
-		System.out.println(getProductView(categoryNumber));
+		showProductsInCategory(categoryNumber);
 	}
 
-	private String getProductView(int categoryNumber) {
-		return Category.getCategoryItems(categoryNumber)
+	private void showProductsInCategory(int categoryNumber) {
+		Category.getCategoryItems(categoryNumber)
 			.stream()
-			.map(product -> product.toString() + "\n")
-			.collect(Collectors.joining());
+			.map(Product::toString)
+			.forEach(System.out::println);
 	}
 }
