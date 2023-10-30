dad300dca7ae32437f89840eea3f314fa3292b94
barami62 <wodbs7758@naver.com>
2023-10-30 16:37:40 +0900

    feat : 장바구니 추가

---
+++ src/app/order/Cart.java
@@ -0,0 +1,27 @@
+package app.order;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import app.domain.Item;
+
+public class Cart {
+
+	private final List<Item> itemList = new ArrayList<>();
+
+	public void add(Item item) {
+		itemList.add(item);
+	}
+
+	public List<Item> getAllItems() {
+		return new ArrayList<>(itemList);
+	}
+
+	public void clear() {
+		itemList.clear();
+	}
+
+	public boolean isEmpty() {
+		return itemList.isEmpty();
+	}
+}
